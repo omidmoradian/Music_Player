0913917ad1f488b717e8113e9568904fb8275a6b
omidmoradi1512 <omidmoradi1512@gmail.com>
2025-10-18 11:59:12 +0330

    Initial commit: add project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <div>
+                <h4>omid1</h4>
+            </div>
+        </>
+    )
+}
+
+export default App
